9ab6526bae676eb3328740fb05c06c3afdea55f9
Paul Brubaker <pbrubak@gmail.com>
2021-06-24 17:40:15 -0700

    retry commit

--- app.js
+++ app.js
@@ -7,3 +7,38 @@
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+
+const catSound = document.getElementById("cat-sound");
+const catImage = document.getElementById("cat-image");
+catImage.addEventListener('click', () => {
+  catSound.play();
+});
+
+const dogSound = document.getElementById("dog-sound");
+const dogImage = document.getElementById("dog-image");
+dogImage.addEventListener('click', () => {
+  dogSound.play();
+});
+
+const horseSound = document.getElementById("horse-sound");
+const horseImage = document.getElementById("horse-image");
+horseImage.addEventListener('click', () => {
+  horseSound.play();
+});
+
+// document.addEventListener('keydown', (event) => {
+//   var name = event.key;
+//   var code = event.code;
+//   // Alert the key name and key code on keydown
+//   alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+// }, false);
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'd') {
+    dogSound.play();
+  } else if (event.key === 'h') {
+    horseSound.play();
+  } else if (event.key === 'c') {
+    catSound.play();
+  }
+});
